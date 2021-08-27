6fbef52b2298dbdae2614bcbc1c86dfd1268e453
manasnisar <anasnisar980@gmail.com>
2021-08-27 17:19:28 +0500

    pushing to gi

---
+++ src/portfolio.js
@@ -0,0 +1,350 @@
+/* Change this file to get your personal Portfolio */
+
+// To change portfolio colors globally go to the  _globalColor.scss file
+
+// Summary And Greeting Section
+
+import emoji from "react-easy-emoji";
+
+const illustration = {
+  animated: true // set to false to use static SVG
+};
+
+const greeting = {
+  username: "Muhammad Anas Nisar",
+  title: "Hi, I'm Anas",
+  subTitle: "A passionate Full Stack Developer experienced in building Web applications with JavaScript / Reactjs / Nodejs and some other cool libraries and frameworks.",
+  resumeLink:
+    "https://docs.google.com/document/d/1J12UgX7g6Td26Te1zJz-_CqMhy2vJkSJ/edit?usp=sharing&ouid=115776562961234848638&rtpof=true&sd=true",
+  displayGreeting: true // Set false to hide this section, defaults to true
+};
+
+// Social Media Links
+
+const socialMediaLinks = {
+  github: "https://github.com/manasnisar",
+  linkedin: "https://www.linkedin.com/in/anas-nisar-8944441a1",
+  gmail: "anasnisar980@gmail.com",
+  stackoverflow: "https://stackoverflow.com/users/10569660/anas-nisar",
+  // Instagram and Twitter are also supported in the links!
+  display: true // Set true to display this section, defaults to false
+};
+
+// Skills Section
+
+const skillsSection = {
+  title: "What I do",
+  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  skills: [
+    emoji(
+      "⚡ Develop highly interactive Front end / User Interfaces for your web applications"
+    ),
+    emoji("⚡ Design, build and improve fault-tolerant API, microservices and cloud architecture to ensure optimal performance and experience"),
+    emoji(
+      "⚡ Integration of third party services such as Firebase/ AWS / GCP/ Braintree"
+    )
+  ],
+
+  /* Make Sure to include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt"
+    },
+    {
+      skillName: "sass",
+      fontAwesomeClassname: "fab fa-sass"
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js"
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "fab fa-react"
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "fab fa-node"
+    },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "fab fa-npm"
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "aws",
+      fontAwesomeClassname: "fab fa-aws"
+    },
+    {
+      skillName: "docker",
+      fontAwesomeClassname: "fab fa-docker"
+    },
+    {
+      skillName: "MongoDB",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "Redis",
+      fontAwesomeClassname: "fas fa-box"
+    },
+    {
+      skillName: "GCP",
+      fontAwesomeClassname: "fas fa-cloud"
+    },
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Education Section
+
+const educationInfo = {
+  display: true, // Set false to hide this section, defaults to true
+  schools: [
+    {
+      schoolName: "Comsats University Islamabad",
+      logo: require("./assets/images/stanfordLogo.png"),
+      subHeader: "Bachelor of Science in Software Engineering",
+      duration: "September 2016 - April 2020",
+      desc: "Ranked top 5% in the program. Took courses about Software Engineering, Web Security, Operating Systems, Visual Programming, Database Designs, Project Management, ...",
+    }
+  ]
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Frontend/Design", //Insert stack or technology you have experience in
+      progressPercentage: "80%" //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Backend",
+      progressPercentage: "90%"
+    },
+    {
+      Stack: "Programming",
+      progressPercentage: "95%"
+    }
+  ],
+  displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+};
+
+// Work experience section
+
+const workExperiences = {
+  display: true, //Set it to true to show workExperiences Section
+  experience: [
+    {
+      role: "Software Engineer",
+      company: "Sectem Technologies",
+      companylogo: require("./assets/images/sectem-logo.png"),
+      date: "August 2019 – Present",
+      descBullets: [
+        "Conceptualizing, designing and building technical solutions using JavaScript",
+        "Working with both SAAS and On-Premises based product development",
+        "Hands on experience with serverless tech stack and microservices architecture",
+        "Following agile methodologies and scrum practices"
+      ]
+    },
+    {
+      role: "MERN Stack Developer",
+      company: "Freelance",
+      companylogo: require("./assets/images/freelance-logo.jpg"),
+      date: "March 2019 – August 2019",
+      descBullets: [
+        "Architected and build solutions for clients on freelance platforms",
+        "Gathered requirements, translated to scalable solutions and deployed over various platforms",
+        "Kept communication alive with clients, entertained revision request and closed contracts gracefully",
+      ]
+    },
+  ]
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Some big projects you have worked on
+
+const bigProjects = {
+  title: "Big Projects",
+  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
+  projects: [
+    {
+      image: require("./assets/images/skipify-logo.jpg"),
+      projectName: "Skipify",
+      projectDesc: "Frictionless Commerce, Secure Omni Channel Payments",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://skipify.com/"
+        }
+        //  you can add extra buttons here.
+      ]
+    },
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Achievement Section
+// Include certificates, talks etc
+
+const achievementSection = {
+  title: emoji("Achievements And Certifications 🏆 "),
+  subtitle:
+    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+
+  achievementsCards: [
+    {
+      title: "",
+      subtitle:
+        "",
+      // image: require(""),
+      footerLink: [
+        {
+          name: "",
+          url: ""
+        },
+        {
+          name: "",
+          url: ""
+        },
+        {
+          name: "",
+          url: ""
+        }
+      ]
+    },
+    {
+      title: "",
+      subtitle:
+        "",
+      // image: require(""),
+      footerLink: [
+        {
+          name: "",
+          url: ""
+        }
+      ]
+    },
+
+    {
+      title: "",
+      subtitle: "",
+      // image: require(""),
+      footerLink: [
+        {name: "Certification", url: ""},
+        {
+          name: "",
+          url: ""
+        }
+      ]
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Blogs Section
+
+const blogSection = {
+  title: "Blogs",
+  subtitle:
+    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+
+  blogs: [
+    {
+      url: "",
+      title: "",
+      description:
+        ""
+    },
+    {
+      url: "",
+      title: "",
+      description:
+        ""
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Talks Sections
+
+const talkSection = {
+  title: "TALKS",
+  subtitle: emoji(
+    "I LOVE TO SHARE MY KNOWLEDGE"
+  ),
+
+  talks: [
+    {
+      title: "",
+      subtitle: "",
+      slides_url: "",
+      event_url: ""
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Podcast Section
+
+const podcastSection = {
+  title: emoji("Podcast 🎙️"),
+  subtitle: "I LOVE TO TALK ABOUT TECHNOLOGY",
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: [
+    ""
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+const contactInfo = {
+  title: emoji("Contact Me ☎️"),
+  subtitle:
+    "Discuss an opportunity or just want to say hi? My Inbox is open for all.",
+  number: "+92-3179665466",
+  email_address: "anasnisar980@gmail.com"
+};
+
+// Twitter Section
+
+const twitterDetails = {
+  userName: "twitter", //Replace "twitter" with your twitter username without @
+  display: false // Set true to display this section, defaults to false
+};
+
+export {
+  illustration,
+  greeting,
+  socialMediaLinks,
+  skillsSection,
+  educationInfo,
+  techStack,
+  workExperiences,
+  openSource,
+  bigProjects,
+  achievementSection,
+  blogSection,
+  talkSection,
+  podcastSection,
+  contactInfo,
+  twitterDetails
+};
