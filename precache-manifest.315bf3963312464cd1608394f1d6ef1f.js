8e55a3b5ffd318ebdfcfd7f3030513e9e533d06f
manasnisar <55254721+manasnisar@users.noreply.github.com>
2022-06-03 16:02:16 +0000

    Deploying to gh-pages from  @ 191321a6ac6b0a1497826494230426e2d9a77b8d 🚀

---
+++ precache-manifest.315bf3963312464cd1608394f1d6ef1f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9e30fd56aa02257cb495c6afe2ee6f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1b5391459d8b319f9d1",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "33814c1ad3d25c733601",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "72f7a02a9ae9e5748d79",
+    "url": "/static/css/main.1a6005f5.chunk.css"
+  },
+  {
+    "revision": "461981dcccc7d30e87d9",
+    "url": "/static/js/2.ab359504.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.ab359504.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1b5391459d8b319f9d1",
+    "url": "/static/js/3.1cfd6387.chunk.js"
+  },
+  {
+    "revision": "33814c1ad3d25c733601",
+    "url": "/static/js/4.e7d2a519.chunk.js"
+  },
+  {
+    "revision": "72f7a02a9ae9e5748d79",
+    "url": "/static/js/main.27e2213b.chunk.js"
+  },
+  {
+    "revision": "5a01c585665f5ba13794",
+    "url": "/static/js/runtime-main.a4e4a6ae.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "52c73b3e1a9913942eb8a6d83c853af5",
+    "url": "/static/media/big-logo.52c73b3e.png"
+  },
+  {
+    "revision": "925b5a01ac6a12940870d5b2f4a829e1",
+    "url": "/static/media/comsats-logo.925b5a01.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e06972851a9a1cd5b66cedfb7fc070b7",
+    "url": "/static/media/freelance-logo.e0697285.jpg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
